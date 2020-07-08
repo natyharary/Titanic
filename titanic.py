617a7cd07daf1140729bb045b36afcd0d00aaf48
natyharary <47058664+natyharary@users.noreply.github.com>
2020-07-08 19:05:34 +0300

    Everything is partitionede into functions

--- titanic.py
+++ titanic.py
@@ -1,70 +1,113 @@
 import pandas as pd
 import xgboost as xgb
 import numpy as np
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
 
 # Load data
 X = pd.read_csv(".\\data\\train.csv")
 y = X['Survived']
-X_test_original = pd.read_csv(".\\data\\test.csv")
-X_test = X_test_original
+X_test = pd.read_csv(".\\data\\test.csv")
 X_train, X_valid, y_train_dummy, y_valid_dummy = train_test_split(X, y, train_size=0.8, test_size=0.2,
                                                                   random_state=np.random)
 
-# Dropping non vital columns
-X_train = X_train.drop('Name', axis=1)
-X_train = X_train.drop('Ticket', axis=1)
-X_train = X_train.drop('Cabin', axis=1)
 
-X_valid = X_valid.drop('Name', axis=1)
-X_valid = X_valid.drop('Ticket', axis=1)
-X_valid = X_valid.drop('Cabin', axis=1)
+def drop_non_vital_columns(dataframe):
+    """
+    Dropping non vital columns
+    :param dataframe: dataframe, raw data
+    :return df: dataframe, reduced dataframe without non-informational columns
+    """
+    df = dataframe
+    df = df.drop('Name', axis=1)
+    df = df.drop('Ticket', axis=1)
+    df = df.drop('Cabin', axis=1)
+    return df
 
-X_test = X_test.drop('Name', axis=1)
-X_test = X_test.drop('Ticket', axis=1)
-X_test = X_test.drop('Cabin', axis=1)
 
-# Treating NaN values
-X_train = X_train.dropna(subset=['Embarked'])  # Drop the few rows with no place of embankment
-X_train['Age'].fillna((X_train['Age'].mean()), inplace=True)  # Imputing missing age with mean value
+def treat_nan_values(dataframe):
+    """
+    Treating NaN values. Imputing with mean value
+    :param dataframe: dataframe, raw data
+    :return df: dataframe, reduced dataframe without nan values
+    """
+    df = dataframe
+    df = df.dropna(subset=['Embarked'])  # Drop the few rows with no place of embankment
+    df['Age'].fillna((dataframe['Age'].mean()), inplace=True)  # Imputing missing age with mean value
+    return df
 
-X_valid = X_valid.dropna(subset=['Embarked'])  # Drop the few rows with no place of embankment
-X_valid['Age'].fillna((X_valid['Age'].mean()), inplace=True)  # Imputing missing age with mean value
 
-# Separating data into different columns
-numerical_cols = [col for col in X_train.columns if
-                  X_train[col].dtype in ['int64', 'float64']]
-categorical_cols = [col for col in X_train.columns
-                    if X_train[col].nunique() < 10 and
-                    X_train[col].dtype == "object"]
+def one_hot_encoding(dataframe):
+    """
+    One hot encoding for categorical columns in the dataframe
+    :param dataframe: raw data
+    :return df: dataframe with onehotencoding
+    """
+    df = dataframe
+    # Separating data into different columns
+    numerical_cols = [col for col in X_train.columns if
+                      X_train[col].dtype in ['int64', 'float64']]
+    categorical_cols = [col for col in X_train.columns
+                        if X_train[col].nunique() < 10 and
+                        X_train[col].dtype == "object"]
 
-# One hot encoding categorical data
-X_train = pd.get_dummies(X_train, prefix_sep="__",
-                         columns=categorical_cols)
-X_valid = pd.get_dummies(X_valid, prefix_sep="__",
-                         columns=categorical_cols)
-X_test = pd.get_dummies(X_test, prefix_sep="__",
+    # One hot encoding categorical data
+    df = pd.get_dummies(df, prefix_sep="__",
                         columns=categorical_cols)
+    return df
+
+
+def modify_data(dataframe):
+    """
+    Apply all data manipulation functions on a dataframe
+    :param dataframe: raw data
+    :return df: manipulated dataframe
+    """
+    df = drop_non_vital_columns(dataframe)
+    df = treat_nan_values(df)
+    df = one_hot_encoding(df)
+    return df
+
+
+def get_y(dataframe):
+    """
+    :param dataframe: raw data
+    :return y: y column
+            dataframe_modified: dataframe without y column
+    """
+    y = dataframe['Survived']
+    dataframe_modified = dataframe.drop('Survived', axis=1)
+    return y, dataframe_modified
+
+X_train_modified = modify_data(X_train)
+X_valid_modified = modify_data(X_valid)
+X_test_modified = modify_data(X_test)
 
 # Get y
-y_train = X_train['Survived']
-X_train = X_train.drop('Survived', axis=1)
-
-y_valid = X_valid['Survived']
-X_valid = X_valid.drop('Survived', axis=1)
+y_train, X_train_modified_no_y = get_y(X_train_modified)
+y_valid, X_valid_modified_no_y = get_y(X_valid_modified)
 
 # Define and fit model
 model = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.03)
-model.fit(X_train, y_train)
+model.fit(X_train_modified_no_y, y_train)
 
-# Check
-y_pred = model.predict(X_test)
-y_pred_valid = model.predict(X_valid)
-mae = mean_absolute_error(y_valid, y_pred_valid)
-print("Mean absolute error for validity is", mae)
+# TODO make cross validation, and graph the iterations with sns
+# Checking accuracy
+y_pred = model.predict(X_test_modified)
+y_pred_valid = model.predict(X_valid_modified_no_y)
+# TODO maybe remove MAE
+# mae = mean_absolute_error(y_valid, y_pred_valid)
+# print("Mean absolute error for validity is", mae)
 
+print(confusion_matrix(y_valid, y_pred_valid))
+print(classification_report(y_valid, y_pred_valid))
+acc = accuracy_score(y_valid, y_pred_valid)
 
-submission = pd.DataFrame({'PassengerId': X_test['PassengerId'],
+submission = pd.DataFrame({'PassengerId': X_test_modified['PassengerId'],
                            'Survived': y_pred})
 submission.to_csv("submission.csv", index=False)
